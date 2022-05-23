63df9a8aa4b5a5d10d04797b7e54a8ea953af5e7
XCO_Yoshi <joshuakeggenhoff@gmail.com>
2022-05-23 23:13:47 +0200

    Würfel Hinzugefügt
    Added basic roll comand

---
+++ commands/3D20.js
@@ -0,0 +1,22 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = 
+{
+	data: new SlashCommandBuilder() // Comand REG
+		.setName('3d20')
+		.setDescription('Würfelt 3 D20 Würfel'),
+
+	async execute(interaction) // Funktion des Comands
+	{
+        const rolle1 = getRandomArbitrary(1, 20)
+		const rolle2 = getRandomArbitrary(1, 20)
+		const rolle3 = getRandomArbitrary(1, 20)
+		return interaction.reply({ content: `du hast eine: ${rolle1} ${rolle2} ${rolle3} Gewürfelt.`,});
+	},
+};
+
+function getRandomArbitrary(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
