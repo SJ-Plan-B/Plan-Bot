0b27fd2cc25bb45d1cbfe3ec0b1d66caac3ceb92
xCesir <Sidney.Schmidt@gmx.de>
2022-05-23 19:40:24 +0200

    v.1.0.4
    add play cmd

--- commands/play.js
+++ commands/play.js
@@ -9,11 +9,22 @@ module.exports =
 
 	async execute(interaction)
 	{
+<<<<<<< Updated upstream
 		const url = interaction.options.getInteger('url');
 
 		
 
 		interaction.reply({ content: `Successfully pruned \`${url}\` messages.`, ephemeral: true });
 		return true;
+=======
+		const url = interaction.options.getString('url');
+		const voiceChannel = interaction.member.voice.channel;
+		if (!voiceChannel)
+		  return interaction.reply(
+			"You need to be in a voice channel to play music!"
+		  );
+		interaction.reply('playing '+url)
+		return (url, interaction);
+>>>>>>> Stashed changes
 	},
 };
