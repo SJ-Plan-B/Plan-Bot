fa5683768bcc9895e7cefb3ff2022b750b60611a
XCO_Yoshi <joshuakeggenhoff@gmail.com>
2022-05-27 23:02:04 +0200

    added Weolcom / leave message

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,20 +1,51 @@
 const {Message, MessageEmbed} = require('discord.js');
+const { message_welcome ,message_welcome_header, message_welcome_chanel, message_welcome_embed_collor } =require('../data/config.json')
 
 module.exports = {
 	name: "guildMemberAdd",
+	
 	async execute(member) {
-		member.guild.channels.cache.get("979796261127483433").send(`${member.user} Willkommen in der Apotheke Plan B. Hier erhalten Sie ausschließlich die "Pille Danach", die auch gegen Menschen funktionieren, die schon lange geboren sind.`);
-		console.log(member.user);
-
-		const newMemberEmbed = new MessageEmbed()
-			.setColor("#d81e5b")
-			.setTitle("New Member!")
-			.setDescription(`${member.user} Willkommen in der Apotheke Plan B. Hier erhalten Sie ausschließlich die "Pille Danach", die auch gegen Menschen funktionieren, die schon lange geboren sind.`)
+		var nickname = getname(member)
+		try {
+			const newMemberEmbed = new MessageEmbed()
+			.setColor(message_welcome_embed_collor)
+			.setTitle(message_welcome_header)
+			.setAuthor({
+					name: nickname
+			})
+			.setDescription(message_welcome)
 			.setThumbnail(member.user.displayAvatarURL())
 			.setTimestamp();
 		
-			member.guild.channels.cache.get("979796261127483433").send({
+			member.guild.channels.cache.get(message_welcome_chanel).send({
 				embeds: [newMemberEmbed] 
 			})
+		} catch (error) {
+			console.warn('Error while performing guildMemberAdd')
+			console.error(error)
+		}	
+
 	}
 }
+
+try {
+	function getname(member){
+		let memberarry = []
+		memberarry = member.user;
+			const myJSON = JSON.stringify(memberarry);
+			const myArray = myJSON.split(",")
+			const slicerdicer = myArray[4];
+			let membername = slicerdicer.slice(11);
+			let result = membername.replace(/["]/g, '')
+			return result;	
+	}	
+} catch (error) {
+	console.warn('Error while performing getname in guildMemberAdd')
+	console.error(error)
+}
+
+
+
+
+
+
--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,8 +1,45 @@
 const {Message, MessageEmbed} = require('discord.js');
+const { message_leave_1, message_leave_2, message_leave_header, message_leave_chanel, message_leave_embed_collor } =require('../data/config.json')
 
 module.exports = {
 	name: "guildMemberRemove",
 	async execute(member) {
-		member.guild.channels.cache.get("979796297169113108").send(`${member.user} has left the server!`);
+		var nickname = getname(member)
+
+		try {
+			const newMemberEmbed = new MessageEmbed()
+			.setColor(message_leave_embed_collor)
+			.setTitle(message_leave_header)
+			.setAuthor({
+					name: nickname
+			})
+			.setDescription(message_leave_1+nickname+message_leave_2)
+			.setThumbnail(member.user.displayAvatarURL())
+			.setTimestamp();
+		
+			member.guild.channels.cache.get(message_leave_chanel).send({
+				embeds: [newMemberEmbed] 
+			})
+		} catch (error) {
+			console.warn('Error while performing guildMemberRemove')
+			console.error(error)
+		}	
+
 	}
+}
+
+try {
+	function getname(member){
+		let memberarry = []
+		memberarry = member.user;
+			const myJSON = JSON.stringify(memberarry);
+			const myArray = myJSON.split(",")
+			const slicerdicer = myArray[4];
+			let membername = slicerdicer.slice(11);
+			let result = membername.replace(/["]/g, '')
+			return result;	
+	}	
+} catch (error) {
+	console.warn('Error while performing getname in guildMemberRemove')
+	console.error(error)
 }
