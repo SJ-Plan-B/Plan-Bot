cf0f38bf29dc3742e8590c2cf16276f423e8d402
XCO_Yoshi <joshuakeggenhoff@gmail.com>
2022-05-29 17:42:30 +0200

    Fixes
    Fixt crashes in play funktions in the fun comands

--- commands/fuchs.js
+++ commands/fuchs.js
@@ -12,17 +12,25 @@ module.exports =
 		try {
 			const channel = interaction.member.voice.channel;
 			const song = 'https://www.youtube.com/watch?v=QwZRRsh6khA'
-			try{
-				music.play({
-					interaction: interaction,
-					channel: channel,
-					song: song
-					});
-				interaction.reply(`${await(interaction.user.username)} hat die Gans gestohlen`);
-				return song;
-			}catch(error){
-				interaction.reply('Invalide Song Link');
-			}
+
+			switch(true){
+				case(channel === null):
+					interaction.reply('You must be in a Voicechannel');
+					break;
+	
+				case(song === null):
+					interaction.reply('no song in Queue');
+					break;
+	
+				default:
+					try{
+						music.play({ interaction: interaction, channel: channel, song: song});
+						interaction.reply(`${await(interaction.user.username)} hat die Gans gestohlen`);
+						return song;
+					}catch(error){
+						console.info('Error while performing play')
+						interaction.reply('Invalide Song Link');
+					}}
 		} catch (error) {
 			console.warn('Error while performing play')
 			console.error(error)
--- commands/ganz.js
+++ commands/ganz.js
@@ -12,17 +12,25 @@ module.exports =
 		try {
 			const channel = interaction.member.voice.channel;
 			const song = 'https://www.youtube.com/watch?v=_LGXSnHtq8Q'
-			try{
-				music.play({
-					interaction: interaction,
-					channel: channel,
-					song: song
-					});
-				interaction.reply(`${await(interaction.user.username)} wird vom fuchs gestolen`);
-				return song;
-			}catch(error){
-				interaction.reply('Invalide Song Link');
-			}
+			
+			switch(true){
+				case(channel === null):
+					interaction.reply('You must be in a Voicechannel');
+					break;
+	
+				case(song === null):
+					interaction.reply('no song in Queue');
+					break;
+	
+				default:
+					try{
+						music.play({ interaction: interaction, channel: channel, song: song});
+						interaction.reply(`${await(interaction.user.username)} wird vom fuchs gestolen`);
+						return song;
+					}catch(error){
+						console.info('Error while performing play')
+						interaction.reply('Invalide Song Link');
+					}}
 		} catch (error) {
 			console.warn('Error while performing play')
 			console.error(error)
