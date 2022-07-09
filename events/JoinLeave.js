a3c86a255f48692a9012f974882d4ee53c26381c
XCO_Yoshi <joshuakeggenhoff@gmail.com>
2022-07-09 17:45:20 +0200

    Update JoinLeave.js

--- events/JoinLeave.js
+++ events/JoinLeave.js
@@ -37,22 +37,16 @@ module.exports = {
             }
 
         } else if(oldUserChannel !== null && newUserChannel !== null && oldUserChannel !== newUserChannel){
-          console.log(oldState.channel.name)
-          console.log(newState.channel.name)
             // User change a voice channel
             if (toString(await querychannelcount(newUserChannel)) === 1 && toString(await querychannelcount(oldUserChannel)) === 0){
-              console.log("1")
               channeldupe(newState)
             }else if(toString(await querychannelcount(newUserChannel)) === 0 && toString(await querychannelcount(oldUserChannel)) === 1){
               channeldupe(oldState)
-              console.log("2")
             }else if (toString((await querychannelcount(oldUserChannel)) === 1 && toString(await querychannelcount(newUserChannel)) === 1) && oldState.channel.name !== newState.channel.name){
               channeldupe(newState)
               channeldupe(oldState)
-              console.log("3")
             }else if(toString((await querychannelcount(oldUserChannel)) === 1 && toString(await querychannelcount(newUserChannel)) === 1) && oldState.channel.name == newState.channel.name){
              channeldupe(oldState)
-              console.log("4")
             }else{
              //irrelevant channel 
             }
