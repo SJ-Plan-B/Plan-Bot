951ae1627dcfcbe4f544e14fafa9708a3c7cda95
Sidney <Sidney.Schmidt@gmx.de>
2022-07-10 20:21:37 +0200

    v.1.2.2
    finished rolereaction

--- commands/sendrolereaction.js
+++ commands/sendrolereaction.js
@@ -1,7 +1,7 @@
 const { SlashCommandBuilder,  } = require('@discordjs/builders');
 const { PermissionFlagsBits } = require('discord-api-types/v10');
 const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
-const {client} = require('../index.js')
+const { sendMessage } = require('../index.js')
 const logger = require('../util/logger').log
 const { role_reaction_DB_host, role_reaction_DB_port, role_reaction_DB_user, role_reaction_DB_password, role_reaction_DB_database } =require('../data/db.json')
 var mysql = require('mysql');
@@ -24,69 +24,84 @@ module.exports =
 	async execute(interaction)
 	{
         try{
-			console.log(splitObjIntoArrayOfString(await(getrolelist())))
-
+			const channelId = interaction.channelId
 			let roles = splitObjIntoArrayOfString(await(getrolelist()))
-			console.log(Object.keys(roles).length)
-
+			var first = true
+			var counter = 0
 			var row = new MessageActionRow()
 			for (let index = 0; index < Object.keys(roles).length; index++) {
+				counter++
+				console.log("counter")
+				console.log(counter)
 				row.addComponents(
 				new MessageButton()
 						.setCustomId(roles[index])
 						.setLabel(roles[index])
 						.setStyle('PRIMARY'),
 				)
-			}
-			            
-            const embed = new MessageEmbed()
-			.setColor('#0099ff')
-			.setTitle('Some title')
-			.setURL('https://discord.js.org')
-			.setDescription('Some description here');
-
+				if ((index !==0) && ((index+1)%5 ==0) && first === true) {
+				const embed = new MessageEmbed()
+					.setColor('#0099ff')
+					.setTitle('Some title')
+					.setURL('https://discord.js.org')
+					.setDescription('Some description here');
 		
-		let message = { content: 'Pong!', embeds: [embed], components: [row] }
-		var notRow=[]
-		/*for (let index = 0; index < row.length; index++) {
-			notRow.push(row[index])
-		}
-		message = { components: notRow }*/
-		await interaction.reply(message)
-
+				
+				let message = { content: 'Pong!', embeds: [embed], components: [row] }
+				await interaction.reply(message)
+				first = false
+				row.spliceComponents(0,5)
+				}else if((index !==0) && ((index+1)%5 ==0) && first === false){
+				let message = { content: ' ', embeds: [], components: [row] }
+				await interaction.send(message)
+				}else{
+					if (counter === Object.keys(roles).length) {
+						console.log("hi")
+						let message = { content: ' ', embeds: [], components: [row] }
+						sendMessage(channelId, message)
+					} else {
+					}
+				}
+			}
 		}catch(error){
 				logger.error('Error while performing sendrolereaction');
                 console.log(error)
 		}
-		function getrolelist(){
-			try {
-				var sql = "SELECT DISTINCT name FROM roles";
-				sql = mysql.format(sql);
-				return new Promise((resolve, reject) => {
-				  con.query(sql, (err, result) => {
-					  return err ? reject(err) : resolve(result);
-					}
-				  );
-				}
-			  );
-			} catch (error) {
-			logger.error(`Error while performing 'SELECT' in the database: ${role_reaction_DB_database}, in command listrolereaction`); 
-			}	
-		};
 		
-		function splitObjIntoArrayOfString(obj){
-		
-			let myJSON = JSON.stringify(obj);
-			let myArray = myJSON.split(",")
-		  
-			for (let index = 0; index < Object.keys(myArray).length; index++) {
-				myArray[index] = myArray[index].replace(/{"name":"/, '')
-				myArray[index] = myArray[index].replace(/"}/g, '')
-			}
-		  
-			myArray[0] = myArray[0].replace('[', '')
-			myArray[(Object.keys(myArray).length-1)] = myArray[(Object.keys(myArray).length-1)].replace(']', '')
-			return myArray;
-		  }
 	},
+
 };
+
+function getrolelist(){
+	try {
+		var sql = "SELECT DISTINCT name FROM roles";
+		sql = mysql.format(sql);
+		return new Promise((resolve, reject) => {
+		  con.query(sql, (err, result) => {
+			  return err ? reject(err) : resolve(result);
+			}
+		  );
+		}
+	  );
+	} catch (error) {
+	logger.error(`Error while performing 'SELECT' in the database: ${role_reaction_DB_database}, in command listrolereaction`); 
+	}	
+};
+
+function splitObjIntoArrayOfString(obj){
+	try {
+		let myJSON = JSON.stringify(obj);
+		let myArray = myJSON.split(",")
+	  
+		for (let index = 0; index < Object.keys(myArray).length; index++) {
+			myArray[index] = myArray[index].replace(/{"name":"/, '')
+			myArray[index] = myArray[index].replace(/"}/g, '')
+		}
+	  
+		myArray[0] = myArray[0].replace('[', '')
+		myArray[(Object.keys(myArray).length-1)] = myArray[(Object.keys(myArray).length-1)].replace(']', '')
+		return myArray;
+	} catch (error) {
+		logger.error('Error while performing splitObjIntoArrayOfString in joinleave')
+	}
+  }
--- index.js
+++ index.js
@@ -6,129 +6,137 @@ const {Client, Collection, Intents, Message, Channel, MessageEmbed, GuildMember,
 const {token, guildId} = require('./data/config.json');
 
 
-module.exports = {
-async startbot(){
-
 const client = new Client(
-{intents: [
-			Intents.FLAGS.GUILDS,
-			Intents.FLAGS.GUILD_VOICE_STATES,
-			Intents.FLAGS.GUILD_MESSAGES,
-			Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
-			Intents.FLAGS.DIRECT_MESSAGES,
-			Intents.FLAGS.GUILD_MEMBERS,
-		  ]
-});
-
-client.guilds.cache.get(guildId);
+	{intents: [
+				Intents.FLAGS.GUILDS,
+				Intents.FLAGS.GUILD_VOICE_STATES,
+				Intents.FLAGS.GUILD_MESSAGES,
+				Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+				Intents.FLAGS.DIRECT_MESSAGES,
+				Intents.FLAGS.GUILD_MEMBERS,
+			  ]
+	});
 
 
-//
-// event-handling
-//
-const eventsPath = path.join(__dirname, 'events');
-const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
-
-for (const file of eventFiles) {
-	const filePath = path.join(eventsPath, file);
-	const event = require(filePath);
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args));	
-	}/*else if(event.name === 'JoinLeave'){
-		let returnvalue = await event.execute();
-		console.log('return '+returnvalue);
-	}*/else{
-		client.on(event.name, (...args) => event.execute(...args));
-	}
-}
+module.exports = {
+	async startbot(){
 
 
-//
-// command-handling
-//
-client.commands = new Collection();
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+		//
+		// event-handling
+		//
+		const eventsPath = path.join(__dirname, 'events');
+		const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+		for (const file of eventFiles) {
+			const filePath = path.join(eventsPath, file);
+			const event = require(filePath);
+			if (event.once) {
+				client.once(event.name, (...args) => event.execute(...args));	
+			}/*else if(event.name === 'JoinLeave'){
+				let returnvalue = await event.execute();
+				console.log('return '+returnvalue);
+			}*/else{
+				client.on(event.name, (...args) => event.execute(...args));
+			}
+		}
 
 
-for (const file of commandFiles) {
-	const filePath = path.join(commandsPath, file);
-	const command = require(filePath);
-	client.commands.set(command.data.name, command);
-}
+		//
+		// command-handling
+		//
+		client.commands = new Collection();
+		const commandsPath = path.join(__dirname, 'commands');
+		const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
 
-client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) return;
-	const command = client.commands.get(interaction.commandName);
-	const channelID = interaction.channel.id;
-	const channel = interaction.channel;
-	if (!command) return;
+		for (const file of commandFiles) {
+			const filePath = path.join(commandsPath, file);
+			const command = require(filePath);
+			client.commands.set(command.data.name, command);
+		}
 
-	try {
-		if (interaction.commandName === 'prune') {
+
+		client.on('interactionCreate', async interaction => {
+			if (!interaction.isCommand()) return;
+			const command = client.commands.get(interaction.commandName);
+			const channelID = interaction.channel.id;
+			const channel = interaction.channel;
+			if (!command) return;
+
+			try {
+				if (interaction.commandName === 'prune') {
+					
+					let returnvalue = await command.execute(interaction);
+					logger.info('prune!'+ returnvalue);
+					if (returnvalue===true) {logger.info('channelID '+ channelID); sendMessage(channelID);}
+
+				}else{
+					const worker = new Worker('./commands/'+interaction.commandName+'.js', {workerData: await(command.execute(interaction))});
+
+					//worker.postMessage(interaction);
+
+					worker.on('message', result => {logger.debug('worker'+ result)});
 			
-			let returnvalue = await command.execute(interaction);
-			 logger.info('prune!'+ returnvalue);
-			 if (returnvalue===true) {logger.info('channelID '+ channelID); sendMessage(channelID);}
+					worker.on('error', error => {logger.error('worker error'+ error)});
+			
+					worker.on('exit', exitCode => {
+					if(exitCode != 0)
+					{logger.verbose(exitCode)}
+					;})
+				}
+				
+			} catch (error) {
+				logger.error(error);
+				await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+			}
+		});
 
-		}else{
-			const worker = new Worker('./commands/'+interaction.commandName+'.js', {workerData: await(command.execute(interaction))});
+		/*
+		//
+		// chanel-logger-handling
+		//
+		try {
+			const loggerPath = path.join(__dirname, 'logger');
+			const loggerFiles = fs.readdirSync(loggerPath).filter(file => file.endsWith('.js'));
 
-			//worker.postMessage(interaction);
+			for (const file of loggerFiles) {
+				const filePath = path.join(loggerPath, file);
+				const logger = require(filePath);
+				if (logger.once) {
+					client.once(logger.name, (...args) => logger.execute(...args));	
+				}else{
+					client.on(logger.name, (...args) => logger.execute(...args));
+				}
+			}
 
-			worker.on('message', result => {logger.debug('worker'+ result)});
-	
-			worker.on('error', error => {logger.error('worker error'+ error)});
-	
-			worker.on('exit', exitCode => {
-			if(exitCode != 0)
-			{logger.verbose(exitCode)}
-			;})
+			
+		} catch (error) {
+			logger.error(error)
+			logger.warn('Error Wile Using Logger Funktions')
 		}
-		
-	} catch (error) {
-		logger.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		*/
+
+
+		//
+		//send message to channel by id
+		//
+
+
+		//
+		//Login
+		//
+		client.login(token);
+	},
+
+	async sendMessage(cID,message){
+			try {
+				const channel = cID;
+				client.channels.cache.get(channel).send(message);	
+			} catch (error) {
+				logger.error('Error while performing sendMessage in Index')
+				console.log(error)
+			}
 	}
-});
 
-/*
-//
-// chanel-logger-handling
-//
-try {
-	const loggerPath = path.join(__dirname, 'logger');
-	const loggerFiles = fs.readdirSync(loggerPath).filter(file => file.endsWith('.js'));
-
-	for (const file of loggerFiles) {
-		const filePath = path.join(loggerPath, file);
-		const logger = require(filePath);
-		if (logger.once) {
-			client.once(logger.name, (...args) => logger.execute(...args));	
-		}else{
-			client.on(logger.name, (...args) => logger.execute(...args));
-		}
-	}
-
-	
-} catch (error) {
-	logger.error(error)
-	logger.warn('Error Wile Using Logger Funktions')
-}
-*/
-
-
-//
-//send message to channel by id
-//
-async function sendMessage(cID,message){
-	const channel = cID;
-	 client.channels.cache.get(channel).send(message);	
-};
-//
-//Login
-//
-client.login(token);
-},
 }
