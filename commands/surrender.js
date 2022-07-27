465d6e9e659ac2319907a31ed8257a15c45ac5e5
XCO_Yoshi <joshuakeggenhoff@gmail.com>
2022-07-27 16:51:52 +0200

    added surrender count
    Co-Authored-By: CompagGC <71564886+CompagGC@users.noreply.github.com>

--- commands/surrender.js
+++ commands/surrender.js
@@ -5,7 +5,7 @@ const { MessageEmbed } = require('discord.js');
 const { command_surrender_song_link, command_surrender_picture_link } =require('../data/comand.json')
 
 module.exports = 
-{
+{ 
 	data: new SlashCommandBuilder()
 		.setName('surrender')
 		.setDescription('surrenders (musik)'),
@@ -13,13 +13,20 @@ module.exports =
 	async execute(interaction)
 	{
 		try {
+			var datei = path.join(__dirname, '..', 'data', 'counter.json')
+			var { surrendercounter } = JSON.parse(fs.readFileSync(datei, 'utf8'))
 			const channel = interaction.member.voice.channel;
 			const song = command_surrender_song_link
+			let jsonfile = 'counter.json'
+			let jsonsubfolder = 'data'
+			let jsonvariable = 'surrendercounter'
+			let newcountervalue = surrendercounter+1
 
 			const surrenderEmbed = new MessageEmbed()
 						.setColor('#e30926')
 						.setTitle('Surrender')
-						.setDescription(`${await(interaction.user.username)} hat surrenderd`)
+						.setDescription(`${await(interaction.user.username)} hat surrenderd.
+										es wurde bereits ${newcountervalue} Aufgegeben.`)
 						.setThumbnail(command_surrender_picture_link)
 
 			switch(true){
@@ -33,8 +40,11 @@ module.exports =
 	
 				default:
 					try{
+						let output = Number((newcountervalue))
+        			    let counted = cfs.writetojsonvariabl(jsonvariable, output, jsonfile, jsonsubfolder)^
+
 						music.play({ interaction: interaction, channel: channel, song: song});
-						return interaction.reply({ embeds: [surrenderEmbed] });
+						if(counted === true) interaction.reply({ embeds: [surrenderEmbed] });
 
 					}catch(error){
 						logger.info('Error while performing play')
