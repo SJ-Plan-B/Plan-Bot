fba799d293eb42fb7bbabb2b20280d343e66c0e9
XCO_Yoshi <joshuakeggenhoff@gmail.com>
2022-08-02 22:28:57 +0200

    Update customfunctions.js

--- util/customfunctions.js
+++ util/customfunctions.js
@@ -8,6 +8,7 @@ const path = require('path');
 module.exports = {
         writetojsonvariabl(variablenname, wertdervariable, jsondateiname, jsonsubfoldername){ 
             //  if inputing a number use let output = Number((zahlvariable)) in host funktion
+            // input must be a valid jason variabel input
          
             var datei
             if (jsonsubfoldername !== null) {
