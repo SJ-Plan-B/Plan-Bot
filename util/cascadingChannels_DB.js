e7e00708a00744fbff972979b07fe58e8380262c
XCO_Yoshi <joshuakeggenhoff@gmail.com>
2022-08-31 14:28:10 +0200

    Update cascadingChannels_DB.js

--- util/cascadingChannels_DB.js
+++ util/cascadingChannels_DB.js
@@ -23,10 +23,9 @@ module.exports = {
                 if (err) throw err;
                 logger.http(`Table channels created in database: ${cascadingChannels_DB_database}`);
               });
+              con.end(function(err) {
+                logger.http(`a Connection to database: ${cascadingChannels_DB_database} has been terminated`);
+              });
         });
-
-        con.end(function(err) {
-            logger.http(`a Connection to database: ${cascadingChannels_DB_database} has been terminated`);
-          });
     }
 }
