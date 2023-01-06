fc2c060ed4340254714045ebb4e8fa619802bda2
XCO_Yoshi <joshuakeggenhoff@gmail.com>
2023-01-06 12:57:41 +0100

    Update index.js
    Formating

--- index.js
+++ index.js
@@ -40,6 +40,7 @@ module.exports = {
 		//
 		// event-handling
 		//
+
 		const eventsPath = path.join(__dirname, 'events');
 		const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
@@ -57,6 +58,7 @@ module.exports = {
 		//
 		// command-handling
 		//
+
 		client.commands = new Collection();
 		const commandsPath = path.join(__dirname, 'commands');
 		const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
@@ -110,6 +112,7 @@ module.exports = {
 		//
 		//Login
 		//
+		
 		client.login(token);
 	},
 
