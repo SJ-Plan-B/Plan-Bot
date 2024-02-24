d9ab19b6b59a10617f2156db67aa7ea41e1ff76b
XCO_Yoshi <26392051+XCOYoshi@users.noreply.github.com>
2024-02-24 12:14:34 +0100

    1.4.9 Updated Dependency
    * Update package-lock.json
    
    * added function to check if data dictionary is present.
    Updated dependency

--- build.js
+++ build.js
@@ -1,6 +1,19 @@
+const { log } = require('./util/logger');
 const { setconfig } = require('./util/yaml-converter');
+const fs = require('node:fs');
 const logger = require('./util/logger').log;
 
+// check if DATA dir Exists
+let folderName = './data';
+
+try {
+  if (!fs.existsSync(folderName)) {
+    fs.mkdirSync(folderName);
+  }
+} catch (err) {
+  log.error(err);
+}
+
 // config modul
-logger.info('Starting Config phraser')
+logger.info('Starting Config phrasing')
 setconfig()
